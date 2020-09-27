c705f447937df8b065f3313c67fa894ba8f1b2a4
panx27 <panx27@gmail.com>
2020-09-27 11:45:24 -0500

    update mongodb/fetch.py

--- wikidata/mongodb/fetch.py
+++ wikidata/mongodb/fetch.py
@@ -1,51 +1,133 @@
 import logging
+import functools
+
+from pymongo import MongoClient
 
 
 logger = logging.getLogger()
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+logging.root.setLevel(level=logging.INFO)
 
 
-def get_all_properties(collection):
-    pid2name = {}
-    query = {'_id': {'$regex': '^P\d+'}}
-    for i in collection.find(query):
+class Wikidata(object):
+    def __init__(self, host, port, db_name, collection_name):
+        self.client = MongoClient(host=host, port=port)
+        self.collection = self.client[db_name][collection_name]
+
+    def get_p31s(self, qid):
+        """
+        """
+
+        query = {'id': qid}
+        project = {
+            '_id': 0,
+            'id': 1,
+            'claims.P31.mainsnak.datavalue.value.id': 1
+        }
+        res = []
+        r = self.collection.find_one(query, project)
+        if r:
+            try:
+                for i in r['claims']['P31']:
+                    p31 = i['mainsnak']['datavalue']['value']['id']
+                    res.append(p31)
+            except KeyError:
+                pass
+        return res
+
+    def get_label(self, qid, lang='en'):
+        """
+        """
+
+        query = {'id': qid}
+        project = {
+            '_id': 0,
+            f'labels.{lang}.value': 1,
+            'id': 1,
+        }
+        res = None
+        r = self.collection.find_one(query, project)
         try:
-            pid2name[i['_id']] = i['labels']['en']['value']
-        except KeyError:
-            logger.error('Cannot find the English label name for %s' % i['_id'])
+            return r['labels'][lang]['value']
+        except (KeyError, TypeError):
+            return None
 
-    return pid2name
+    def get_wiki(self, qid, wikisite='enwiki'):
+        """
+        """
+
+        query = {'id': qid}
+        project = {
+            '_id': 0,
+            f'sitelinks.{wikisite}.title': 1,
+            'id': 1
+        }
+        r = self.collection.find_one(query, project)
+        try:
+            return r['sitelinks'][wikisite]['title']
+        except (KeyError, TypeError):
+            return None
+
+    def get_description(self, qid, lang='en'):
+        """
+        """
+
+        query = {'id': qid}
+        project = {
+            '_id': 0,
+            f'descriptions.{lang}.value': 1,
+            'id': 1,
+        }
+        try:
+            r = collection.find_one(query, project)
+            return r['descriptions'][lang]['value']
+        except (KeyError, TypeError):
+            return None
+
+    def get_qids_p31(self, p31):
+        """
+        """
+
+        query = {'claims.P31.mainsnak.datavalue.value.id': p31}
+        project = {
+            '_id': 0,
+            'claims.P31.mainsnak.datavalue.value.id': 1,
+            'id': 1
+        }
+        res = []
+        for i in self.collection.find(query, project):
+            res.append(i['id'])
+        return res
+
+    def get_qid_wiki(self, wiki, wikisite='enwiki'):
+        """
+        """
+
+        wiki = wiki.replace('_', ' ')
+        query = {f'sitelinks.{wikisite}.title': wiki}
+        project = {
+            '_id': 0,
+            f'sitelinks.{wikisite}.title': 1,
+            'id': 1
+        }
+        # TO-DO: may have 1 to n mapping, return list?
+        r = self.collection.find_one(query, project)
+        try:
+            return r['id']
+        except (KeyError, TypeError):
+            return None
 
 
-def get_description(collection, _id, lang='en'):
-    try:
-        trans = {'descriptions.%s.value' % lang: 1, 'id': 1, '_id': 0}
-        r = collection.find_one({'id': _id}, trans)
-        return r['descriptions'][lang]['value']
-    except (KeyError, TypeError):
-        return None
+    def get_all_properties(self):
+        """
+        """
 
-
-def get_wiki_title(collection, _id, wikisite='enwiki'):
-    try:
-        trans = {'sitelinks.%s.title' % wikisite: 1, 'id': 1, '_id': 0}
-        r = collection.find_one({'id': _id}, trans)
-        return r['sitelinks']['enwiki']['title']
-    except (KeyError, TypeError):
-        return None
-
-
-def get_label_name(collection, _id, lang='en'):
-    try:
-        trans = {'labels.%s.value' % lang: 1, 'id': 1, '_id': 0}
-        r = collection.find_one({'id': _id}, trans)
-        return r['labels'][lang]['value']
-    except (KeyError, TypeError):
-        return None
-
-
-def get_all_p31(collection, _id):
-    p31 = []
-    r = collection.find_one({'id': _id})
-    for i in r['claims']['P31']:
-        p31.append(j['mainsnak']['datavalue']['value']['id'])
-    return p31
+        pid2name = {}
+        query = {'_id': {'$regex': '^P\d+'}}
+        for i in self.collection.find(query):
+            try:
+                pid2name[i['_id']] = i['labels']['en']['value']
+            except KeyError:
+                msg = f'Cannot find the English label name for {i["_id"]}'
+                logger.error(msg)
+        return pid2name
