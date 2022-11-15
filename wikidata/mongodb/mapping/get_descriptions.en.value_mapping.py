548cf332a81f26f48637dfcac7127e87fabf2af4
panx27 <panx27@gmail.com>
2022-11-14 23:51:09 -0800

    add get_descriptions.en.value_mapping.py

---
+++ wikidata/mongodb/mapping/get_descriptions.en.value_mapping.py
@@ -0,0 +1,64 @@
+import sys
+import os
+import logging
+from itertools import islice
+import multiprocessing
+import pprint
+from collections import defaultdict
+import argparse
+
+import ujson as json
+from pymongo import MongoClient
+from tqdm import tqdm
+
+
+logger = logging.getLogger()
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+logging.root.setLevel(level=logging.INFO)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host', help='MongoDB host')
+    parser.add_argument('port', help='MongoDB port')
+    parser.add_argument('db_name', help='Database name')
+    parser.add_argument('collection_name', help='Collection name')
+    parser.add_argument('output_path', help='Output Path')
+    parser.add_argument('--username', '-u', default=None,
+                        help='Username (if authentication is enabled)')
+    parser.add_argument('--password', '-p', default=None,
+                        help='Password (if authentication is enabled)')
+    args = parser.parse_args()
+
+    host = args.host
+    port = int(args.port)
+    db_name = args.db_name
+    collection_name = args.collection_name
+    output_path = args.output_path
+    username = args.username
+    password = args.password
+
+    logger.info(f'db name: {db_name}')
+    logger.info(f'collection name: {collection_name}')
+    if username and password:
+        client = MongoClient(host=host, port=port,
+                             username=username, password=password)
+    else:
+        client = MongoClient(host=host, port=port)
+
+    collection = client[db_name][collection_name]
+
+    # Do not use {'$exists': True}
+    query = {'descriptions.en.value': {'$regex': '.+'}}
+    project = {'descriptions.en.value': 1, 'id': 1, '_id': 0}
+    res = collection.find(query, project)
+    # stats = res.explain()["executionStats"]
+    # pprint.pprint(stats)
+    # total = stats['nReturned']
+    total = collection.count_documents(query)
+    qid2desc= {}
+    for i in tqdm(res, total=total):
+        qid2desc[i['id']] = i['descriptions']['en']['value']
+
+    with open(output_path, 'w') as fw:
+        json.dump(qid2desc, fw, indent=4)
